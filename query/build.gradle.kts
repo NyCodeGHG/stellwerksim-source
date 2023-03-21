fa2f645d2db2e711a1ee68fe69de275846c5e356
Marie Ramlow <me@nycode.dev>
2023-03-21 17:20:57 +0100

    fix: add missing sun-xml-ws runtime library

--- query/build.gradle.kts
+++ query/build.gradle.kts
@@ -10,6 +10,7 @@ dependencies {
     implementation("javax.xml.bind", "jaxb-api", "2.3.1")
     implementation("javax.xml.ws", "jaxws-api", "2.3.1")
     implementation("javax.jws", "javax.jws-api", "1.1")
+    runtimeOnly("com.sun.xml.ws", "rt", "4.0.1")
 }
 
 tasks {

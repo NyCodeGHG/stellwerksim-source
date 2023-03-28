c7720a7f426ad7e0ee5b684d12458dabb94fb6cf
Marie <me@nycode.dev>
2023-03-28 02:49:10 +0200

    Upgrade jaxws-rt to 2.3.2

--- query/build.gradle.kts
+++ query/build.gradle.kts
@@ -10,7 +10,7 @@ dependencies {
     implementation("javax.xml.bind", "jaxb-api", "2.3.1")
     implementation("javax.xml.ws", "jaxws-api", "2.3.1")
     implementation("javax.jws", "javax.jws-api", "1.1")
-    runtimeOnly("com.sun.xml.ws", "jaxws-rt", "2.3.1")
+    runtimeOnly("com.sun.xml.ws", "jaxws-rt", "2.3.2")
 }
 
 tasks {
